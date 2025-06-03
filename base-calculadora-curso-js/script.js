cc7a8129a93c4a5182c5d2051c76399bd0c05572
Jhonny Remolina <asistenteinde@redsis.com>
2025-06-03 12:29:36 -0500

    ejercicio 1 calculadora

---
+++ base-calculadora-curso-js/script.js
@@ -0,0 +1,19 @@
+//agraga a la pantalla el valor seleccionado
+
+function agregar(valor) {
+    document.getElementById('pantalla').value += valor;
+}
+
+
+function borrar() {
+    document.getElementById('pantalla').value = '';
+}
+
+function calcular() {
+    const valorpantalla = document.getElementById('pantalla').value
+    const resultado = eval(valorpantalla)
+    document.getElementById('pantalla').value = resultado;
+}
+
+
+
